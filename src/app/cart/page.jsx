45d183fd589fc9ec9946d655323eb8974e0358a8
harshrao26 <harshurao058@gmail.com>
2025-12-10 12:04:43 +0530

    ...

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,9 @@
 import CartClient from '@/components/customer/CartClient';
 import BestSellerSectionWrapper from '@/components/customer/BestSellerSectionWrapper';
 
+// Force dynamic rendering as BestSellerSectionWrapper fetches from DB
+export const dynamic = 'force-dynamic';
+
 export default function CartPage() {
   return (
     <>
